5b91ba4eeb3d7423cbd74eea6af81717086afdcb
Gilbert J <gilbertjeff05@gmail.com>
2024-10-11 18:12:51 +0700

    feat: updated Main to use AppController

--- src/main/java/com/svx/github/main/Main.java
+++ src/main/java/com/svx/github/main/Main.java
@@ -1,16 +1,18 @@
 package com.svx.github.main;
 
+import com.svx.github.controller.AppController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception {
-
+    public void start(Stage primaryStage) {
+        AppController appController = new AppController(primaryStage);
+        appController.startApp();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }

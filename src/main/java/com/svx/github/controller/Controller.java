50f7197c85eaaf05ca8ccdc72b3b85fcbbed0f02
Gilbert J <gilbertjeff05@gmail.com>
2024-10-16 11:59:20 +0700

    feat: added default styling for all pages

--- src/main/java/com/svx/github/controller/Controller.java
+++ src/main/java/com/svx/github/controller/Controller.java
@@ -3,6 +3,7 @@ package com.svx.github.controller;
 import com.svx.github.view.View;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import java.util.Objects;
 
 public abstract class Controller<T extends View<? extends Parent>> {
     protected final T view;
@@ -15,14 +16,16 @@ public abstract class Controller<T extends View<? extends Parent>> {
         setActions();
     }
 
-    public Scene getView(double width, double height) {
+    public Scene getScene(double width, double height) {
         Scene scene = new Scene(view.getRoot(), width, height);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/svx/github/style/styles.css")).toExternalForm());
 
         if (view.getStyleReference() != null) {
             scene.getStylesheets().add(view.getStyleReference());
         } else {
             System.out.println("No style reference found for " + view.getClass().getSimpleName());
         }
+
         return scene;
     }
 

d38d88f34526794e4d904d85d7faea4a6c8849ee
Gilbert J <gilbertjeff05@gmail.com>
2024-11-23 14:10:58 +0700

    style: added scroll pane + fixed buttons not using the whole width

--- src/main/java/com/svx/github/view/dialog/CloneRepositoryDialogView.java
+++ src/main/java/com/svx/github/view/dialog/CloneRepositoryDialogView.java
@@ -3,11 +3,13 @@ package com.svx.github.view.dialog;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CloneRepositoryDialogView extends DialogView<VBox> {
+    private ScrollPane repositoryListContainer;
     private VBox repositoryList;
     private TextField pathField;
     private Button chooseDirectoryButton;
@@ -19,13 +21,24 @@ public class CloneRepositoryDialogView extends DialogView<VBox> {
 
         HBox titleBar = createTitleBar("Add Local Repository");
 
+        Label selectRepositoryLabel = new Label("Your Repositories");
+        selectRepositoryLabel.getStyleClass().add("dialog-section-title");
+        selectRepositoryLabel.getStyleClass().add("bold");
+
         repositoryList = new VBox(10);
         repositoryList.getStyleClass().add("repository-list");
+
+        repositoryListContainer = new ScrollPane(repositoryList);
+        repositoryListContainer.setFitToWidth(true);
+        repositoryListContainer.getStyleClass().add("scroll-pane");
 
         pathField = new TextField();
         pathField.setPromptText("Enter repository path...");
         chooseDirectoryButton = new Button("Choose");
         chooseDirectoryButton.getStyleClass().add("secondary-button");
+
+        Label destinationPathLabel = new Label("Destination Path");
+        destinationPathLabel.getStyleClass().addAll("bold");
 
         confirmButton = new Button("Clone");
         confirmButton.getStyleClass().add("primary-button");
@@ -40,14 +53,14 @@ public class CloneRepositoryDialogView extends DialogView<VBox> {
         errorLabel = new Label();
         errorLabel.getStyleClass().add("error-label");
 
+        VBox bottomSection = new VBox(10, destinationPathLabel, new HBox(10, pathField, chooseDirectoryButton), errorLabel, buttons);
+        bottomSection.getStyleClass().add("dialog-bottom-section");
+
         root.getChildren().addAll(
                 titleBar,
-                new Label("Select Repository"),
-                repositoryList,
-                new Label("Destination Path"),
-                new HBox(10, pathField, chooseDirectoryButton),
-                errorLabel,
-                buttons
+                selectRepositoryLabel,
+                repositoryListContainer,
+                bottomSection
         );
     }
 

d120ad37b8af7aad994d1d0127539af2d0268ae1
Gilbert J <gilbertjeff05@gmail.com>
2024-11-24 21:13:50 +0700

    chore: added richtextfx dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.svx.github {
     requires com.fasterxml.jackson.databind;
     requires org.kordamp.ikonli.javafx;
     requires java.desktop;
+    requires org.fxmisc.richtext;
 
     exports com.svx.github.main;
 }

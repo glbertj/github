2e34f56c99486badb386c2e09c852e077c1a9c3a
Gilbert J <gilbertjeff05@gmail.com>
2024-12-01 15:05:52 +0700

    refactor + perf: refactored and improved readability

--- src/main/java/com/svx/chess/model/ChessTile.java
+++ src/main/java/com/svx/chess/model/ChessTile.java
@@ -1,4 +1,4 @@
-package com.svx.github.model.game;
+package com.svx.chess.model;
 
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -8,40 +8,42 @@ import javafx.scene.shape.Circle;
 
 public class ChessTile extends StackPane {
     private final ObjectProperty<ChessPiece> pieceProperty;
-    private final int row;
-    private final int col;
     private final SimpleBooleanProperty isValidMove;
     private final SimpleBooleanProperty isEatable;
     private final SimpleBooleanProperty isRecentMove;
 
-    public ChessTile(int row, int col) {
+    public ChessTile() {
         pieceProperty = new SimpleObjectProperty<>();
-        this.row = row;
-        this.col = col;
         isValidMove = new SimpleBooleanProperty(false);
         isEatable = new SimpleBooleanProperty(false);
         isRecentMove = new SimpleBooleanProperty(false);
 
         getStyleClass().add("tile");
-        String tileClass = (row + col) % 2 == 0 ? "green" : "white";
-        getStyleClass().add(tileClass);
 
-        // Valid move indicator
+        addValidMoveIndicator();
+        addEnemyIndicator();
+        addRecentMoveIndicator();
+        addChessPieceImage();
+    }
+
+    private void addValidMoveIndicator() {
         Circle validMoveCircle = new Circle(10);
         validMoveCircle.getStyleClass().add("valid-move-circle");
         validMoveCircle.setVisible(false);
         getChildren().add(validMoveCircle);
         validMoveCircle.visibleProperty().bind(isValidMove);
+    }
 
-        // Enemy indicator (Ring)
+    private void addEnemyIndicator() {
         Circle enemyCircle = new Circle();
         enemyCircle.setRadius(30.0);
         enemyCircle.getStyleClass().add("enemy-circle");
         enemyCircle.setVisible(false);
         getChildren().add(enemyCircle);
         enemyCircle.visibleProperty().bind(isEatable);
+    }
 
-        // Recent move indicator
+    private void addRecentMoveIndicator() {
         isRecentMove.addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 getStyleClass().add("recent");
@@ -49,8 +51,9 @@ public class ChessTile extends StackPane {
                 getStyleClass().remove("recent");
             }
         });
+    }
 
-        // ImageView of the piece
+    private void addChessPieceImage() {
         pieceProperty.addListener((observable) -> {
             ChessPiece newPiece = pieceProperty.get();
             if (newPiece != null) {
@@ -60,27 +63,20 @@ public class ChessTile extends StackPane {
                 getChildren().add(newPiece.getImageView());
             }
         });
-
-        // Resizes
-//        widthProperty().addListener((observable, oldValue, newValue) -> {
-//            double size = Math.min(getWidth(), getHeight());
-//            enemyCircle.setRadius(size / 2);
-//
-//            if (pieceProperty.get() != null) {
-//                pieceProperty.get().getImageView().setFitWidth(size);
-//                pieceProperty.get().getImageView().setFitHeight(size);
-//            }
-//        });
     }
 
-    public int getRow() { return row; }
-    public int getCol() { return col; }
+    private void addSizeAdjustor() {
+        widthProperty().addListener((observable, oldValue, newValue) -> {
+            double size = Math.min(getWidth(), getHeight());
+            // TODO! Implement this
+        });
+    }
+
     public ChessPiece getPiece() { return pieceProperty.get(); }
     public void setPiece(ChessPiece piece) { pieceProperty.set(piece); }
     public boolean isValidMove() { return isValidMove.get(); }
     public void setIsValidMove(boolean isValidMove) { this.isValidMove.set(isValidMove); }
     public boolean isEatable() { return isEatable.get(); }
     public void setIsEatable(boolean isEatable) { this.isEatable.set(isEatable); }
-    public boolean isRecentMove() { return isRecentMove.get(); }
     public void setIsRecentMove(boolean isRecentMove) { this.isRecentMove.set(isRecentMove); }
 }
